7ddd8e12307a3ee2fd8ad436c4cdb45ee1b95a97
John Martin <johnjamesmartin@live.com>
2019-06-05 14:16:48 +0100

    Created and imported components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 function App() {
   return (
     <React.Fragment>
-      <h3>hello from app</h3>
+      <Navbar />
+      <ProductList />
+      <Details />
+      <Cart />
+      <Default />
     </React.Fragment>
   );
 }
