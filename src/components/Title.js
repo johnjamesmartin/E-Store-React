487aee54608386c9974f2e300b03ff9ddc4ee253
John Martin <johnjamesmartin@live.com>
2019-06-06 23:14:31 +0100

    Copy products rather than pass them as a reference to array

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ export default function Title({ name, title }) {
   return (
     <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1 classname="text-capitalize font-weight-bold">
-          {name} <strong classname="text-blue">{title}</strong>
+        <h1 className="text-capitalize font-weight-bold">
+          {name} <strong className="text-blue">{title}</strong>
         </h1>
       </div>
     </div>
--- src/context.js
+++ src/context.js
@@ -10,12 +10,26 @@ class ProductProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: storeProducts,
+      products: [],
       detailProduct: detailProduct
     };
     this.handleDetail = this.handleDetail.bind(this);
     this.addToCart = this.addToCart.bind(this);
+    this.setProducts = this.setProducts.bind(this);
   }
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = { ...tempProducts, singleItem };
+    });
+    this.setState(() => {
+      return { tempProducts };
+    });
+  };
   handleDetail = () => {
     console.log('Hello from detail');
   };
