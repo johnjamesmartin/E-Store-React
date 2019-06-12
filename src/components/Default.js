f4a38ef68acc0c1dfe48f965902997497754bc31
John Martin <johnjamesmartin@live.com>
2019-06-12 20:23:49 +0100

    Complete 404 page for default component

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404 Error</h1>
+
+            <h3>
+              Page&nbsp;
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{' '}
+              does not exist
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
